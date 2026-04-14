a7745b98f6e40e23f4ec3fb2c3ac3fc58b641c04
Nathan Tisdale <nathan@tisdale.info>
2026-04-13 17:33:37 -0700

    Updating week 12 assignment

--- week12/assignment/src/data/projectData.js
+++ week12/assignment/src/data/projectData.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "Self-portrat illustrating emotional state after ICE murders multiple citizens in Minneapolis, with dropped jaw and tearing eyes",
     url: "https://editor.p5js.org/natisdale/full/NLpPcTeEb",
-    image: "./public/images/self-portrait.png",
+    image: "images/self-portrait.png",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "Self-portrat illustrating emotional state after ICE murders multiple citizens in Minneapolis, with dropped jaw and tearing eyes",
     url: "https://editor.p5js.org/natisdale/full/NLpPcTeEb",
-    image: "./public/images/self-portrait.gif",
+    image: "images/self-portrait.gif",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const projects = [
     description:
       "Draws field of flowers with overloaded function based on this drawFlower function. Uses arc to draw sun, with random stroke color for flicker at edge of sun. Export to Gif doesn't work due to how I used background() in setup(), not draw(). Export to PNG captures the scene correctly, but won't show the animated sun flicker",
     url: "https://editor.p5js.org/natisdale/full/AnotherProject",
-    image: "./public/images/flower_scene.png",
+    image: "images/flower_scene.png",
   },
   {
     id: 4,
@@ -29,35 +29,35 @@ export const projects = [
     description:
       "A simple implementation of the classic Pong game for a single player.",
     url: "https://editor.p5js.org/natisdale/full/eUYf_zBp-",
-    image: "./public/images/pong_game.gif",
+    image: "images/pong_game.gif",
   },
   {
     id: 5,
     name: "For Loops, push(), pop(), and translate()",
     description: "Randomly sized and placed ladybugs",
     url: "https://editor.p5js.org/natisdale/full/wcniMR2UX",
-    image: "./public/images/lady-bugs.png",
+    image: "images/lady-bugs.png",
   },
   {
     id: 6,
     name: "Nested and While Loops",
     description: "Vera Molnár Squares app simulator",
     url: "https://editor.p5js.org/natisdale/full/wpB1qEGho",
-    image: "./public/images/vmsquares.gif",
+    image: "images/vmsquares.gif",
   },
   {
     id: 7,
     name: "Iterables, Strings and DOM",
     description: "Ceasar Cipher encoder and decoder",
     url: "https://editor.p5js.org/natisdale/full/XnVWp91FM",
-    image: "./public/images/ceasar-cipher.png",
+    image: "images/ceasar-cipher.png",
   },
   {
     id: 8,
     name: "Arrays, Methods, and Olgorithms",
     description: "Bar chart of Top 10 MotoGP Grand Prix winners (1949-2022)",
     url: "https://editor.p5js.org/natisdale/full/S4ahfF_p8",
-    image: "./public/images/motogp-bar-chart.gif",
+    image: "images/motogp-bar-chart.gif",
   },
   {
     id: 9,
@@ -65,13 +65,13 @@ export const projects = [
     description:
       "Game where you are a motorcyclist splitting lanes on highway.  Use left and right arrow keys to move. Press r to ride again after crashing",
     url: "https://editor.p5js.org/natisdale/full/HAUUyr4SK",
-    image: "./public/images/lane-splitting.gif",
+    image: "images/lane-splitting.gif",
   },
   {
     id: 10,
     name: "Classes and Object-Oriented Programming",
     description: "Updated version of lane-splitting game with classes and OOP",
     url: "https://editor.p5js.org/natisdale/full/LUwYOZU5B",
-    image: "./public/images/lanesplitter.gif",
+    image: "images/lanesplitter.gif",
   },
 ];
