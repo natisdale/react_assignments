9288254438b4e073b67680d3562b804a21fae203
Nathan Tisdale <nathan@tisdale.info>
2026-04-13 17:30:40 -0700

    Updating week 11 exercise

--- week12/exercise/src/data/bookData.js
+++ week12/exercise/src/data/bookData.js
@@ -5,7 +5,7 @@ export const books = [
     author: "José Raúl Capablanca",
     description:
       "Comprehensive guide on chess strategy and tactics written in the early 20th century. This instructional manual is intended for chess enthusiasts looking to improve their understanding of the game. It focuses on the fundamental principles of chess, covering essential topics such as endgames, openings, and the relative value of pieces. The opening portion of the book begins with a preface where Capablanca emphasizes the timeless nature of chess fundamentals, asserting that while tactics may evolve, the core principles remain constant. He outlines the structure of the book, introducing a series of chapters that will explore essential concepts like simple mating techniques, pawn promotion, and strategies in the middle game. This portion sets the stage for a methodical exploration of the game, with Capablanca encouraging readers to practice the basics to build a solid foundation for higher-level play. (This is an automatically generated summary.)",
-    coverImage: "public/books/pg33870.cover.medium.jpg",
+    coverImage: "books/pg33870.cover.medium.jpg",
     projectGutenbergLink: "https://www.gutenberg.org/ebooks/33870",
   },
   {
@@ -14,7 +14,7 @@ export const books = [
     author: "Edward Lasker",
     description:
       "Comprehensive instructional guide on chess tactics and strategies written in the early 20th century. This book is aimed at players looking to enhance their understanding of chess mechanics and improve their gameplay through structured learning. It covers fundamental concepts about the game, including pieces movement, rules, strategies for openings, middle games, and end-games. The opening portion of the book introduces the basic rules of chess and establishes a clear foundation for understanding the game. Lasker outlines the player’s objectives, the positional setup on the board, and how each piece moves according to specific laws, emphasizing the unique characteristics that define their roles. Additionally, he discusses the significance of strategy, encouraging players to cultivate their judgment in evaluating moves rather than simply memorizing techniques. This thorough primer sets the stage for deeper exploration into various strategies and principles that will be discussed in subsequent chapters, aimed at guiding both beginners and more experienced players in their journey through the tactical landscape of chess. (This is an automatically generated summary.)",
-    coverImage: "public/books/pg5614.cover.medium.jpg",
+    coverImage: "books/pg5614.cover.medium.jpg",
     projectGutenbergLink: "https://www.gutenberg.org/ebooks/5614",
   },
   {
@@ -23,7 +23,7 @@ export const books = [
     author: "Howard Staunton",
     description:
       "Instructional manual on the game of chess that was written in the early 20th century. The work aims to teach beginners the fundamentals of chess, focusing on simplifying the complexities of various openings and strategies through analyzed games. At the start of this manual, the preface outlines its purpose as a resource for those learning chess, indicating that many learners have been overwhelmed by the variety of tactics available, particularly regarding openings. It highlights the importance of understanding the power and moves of chess pieces, with illustrations to demonstrate these concepts. The initial chapter discusses the arrangement of the pieces and the chessboard, defining technical terms and providing the fundamental rules of chess to ensure a solid foundation for new players. (This is an automatically generated summary.)",
-    coverImage: "public/books/pg16377.cover.medium.jpg",
+    coverImage: "books/pg16377.cover.medium.jpg",
     projectGutenbergLink: "https://www.gutenberg.org/ebooks/16377",
   },
   {
@@ -33,7 +33,7 @@ export const books = [
     author: "Frederick Milines Edge",
     description:
       "Historical account written in the mid-19th century. The book centers around the life and achievements of Paul Morphy, a prodigious chess player from America, highlighting his journey, matches, and interactions with prominent chess figures in Europe. It provides insight into the chess scene of the time while documenting Morphy's rise as one of the greatest chess talents. The opening of this work introduces Paul Morphy as a young prodigy whose exceptional aptitude for chess was nurtured by his father. His early experiences show how he quickly surpassed his local competition and garnered attention, leading to his participation in the First American Chess Congress. Here, Morphy's stunning victories establish his national reputation. The narrative also begins to touch on his preparations for a trip to Europe, where the chess world awaits to see if he can triumph against seasoned players, setting the stage for a significant chapter in chess history. (This is an automatically generated summary.)",
-    coverImage: "public/books/pg34180.cover.medium.jpg",
+    coverImage: "books/pg34180.cover.medium.jpg",
     projectGutenbergLink: "https://www.gutenberg.org/ebooks/34180",
   },
   {
@@ -42,7 +42,7 @@ export const books = [
     author: "H. E. Bird",
     description:
       "Historical account written in the late 19th century. The book provides a comprehensive overview of the history of chess, tracing its origins, evolution, and significant milestones throughout the centuries. It delves into the cultural and social impact of chess, highlighting key figures and events that have shaped the game. The opening portion of this work introduces readers to the ancient origins of chess, exploring its roots in India and its subsequent spread to Persia and Europe. It discusses the transformation of the game over time, including changes in rules and the development of different styles of play. The narrative also touches on notable historical figures who contributed to the game's popularity and evolution, setting the stage for a deeper exploration of chess history in subsequent chapters. (This is an automatically generated summary.)",
-    coverImage: "public/books/pg4902.cover.medium.jpg",
+    coverImage: "books/pg4902.cover.medium.jpg",
     projectGutenbergLink: "https://www.gutenberg.org/ebooks/4902",
   },
   {
@@ -51,7 +51,7 @@ export const books = [
     author: "William Brett Fishburne",
     description:
       "A chess-themed guidebook published during the modern era of chess literature. This book likely serves as a resource for chess enthusiasts looking to improve their gameplay, specifically focusing on endgame scenarios involving three pieces. The book explores a series of chess positions and strategies pertaining to these limited material situations. The content of the book primarily consists of various FEN notation representations of chess positions, which are laid out to illustrate checkmating patterns when only three pieces are in play. Each position provides an opportunity for readers to practice and analyze different win strategies. By engaging with the material, players can enhance their tactical skills and learn how to maneuver effectively in endgame situations, making this a valuable tool for both novice and experienced chess players seeking to refine their craft. (This is an automatically generated summary.)",
-    coverImage: "public/books/pg4542.cover.medium.jpg",
+    coverImage: "books/pg4542.cover.medium.jpg",
     projectGutenbergLink: "https://www.gutenberg.org/ebooks/4542",
   },
   {
@@ -60,7 +60,7 @@ export const books = [
     author: "William Brett Fishburne",
     description:
       "A chess-themed guidebook published during the modern era of chess literature. This book likely serves as a resource for chess enthusiasts looking to improve their gameplay, specifically focusing on endgame scenarios involving four pieces. The book explores a series of chess positions and strategies pertaining to these limited material situations. The content of the book primarily consists of various FEN notation representations of chess positions, which are laid out to illustrate checkmating patterns when only four pieces are in play. Each position provides an opportunity for readers to practice and analyze different win strategies. By engaging with the material, players can enhance their tactical skills and learn how to maneuver effectively in endgame situations, making this a valuable tool for both novice and experienced chess players seeking to refine their craft. (This is an automatically generated summary.)",
-    coverImage: "public/books/pg4656.cover.medium.jpg",
+    coverImage: "books/pg4656.cover.medium.jpg",
     projectGutenbergLink: "https://www.gutenberg.org/ebooks/4656",
   },
 ];
